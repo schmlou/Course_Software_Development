453f8cea3f13d4dd00011d48e60df44d9b343ea2
schmlou <louisschmalisch@gmail.com>
2025-11-25 15:35:29 +0100

    started with assignment 2

--- assignment_02/Main.java
+++ assignment_02/Main.java
@@ -4,7 +4,21 @@ package assignment_02;
 public class Main{
 	
 	static double compare (double [][] arr) {
+		
+		
+//Get the distance of the different coordinates from the input: d = sqr((xi-xj)² + (yi-yj)²)
+		
+		
+		
+//Calculate the weights with the calculated distance: w = 1/(d with power P) and the power
+		
+		
+		
+//Get the weighted sum from w = weight and z = Values at coordinates
+		
+		
+
+//The final estimation Z(P) = weighted Sum/weight
 		return 0;
-		//testing
 	}
 }
