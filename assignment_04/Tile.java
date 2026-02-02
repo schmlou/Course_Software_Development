9100a6b5f3b483f8bcb0d1f7696e43c2d345b341
schmlou <louisschmalisch@gmail.com>
2026-02-02 17:45:36 +0100

    removed setters, because i read the instructions again ;)

--- assignment_04/Tile.java
+++ assignment_04/Tile.java
@@ -6,14 +6,6 @@ public class Tile {
 	private boolean isWall;
 	private boolean isExit;
 	
-	//set that tile should be wall
-	public void setWall (boolean wall) {
-		this.isWall = wall;
-	}
-	//set that tile should be exit
-	public void setExit(boolean exit) {
-		this.isExit = exit;
-	}
 	//check if this tile is a Wall
 	public boolean getWall() {
 		return this.isWall;
